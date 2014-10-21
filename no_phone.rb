e5e97238aa1f050cffd1b500dd341fc6e999567d
Daniel Morrison <daniel@collectiveidea.com>
2014-10-20 22:04:32 -0400

    Very basic SIP testing app

--- no_phone.rb
+++ no_phone.rb
@@ -1,3 +1,21 @@
 class NoPhone < Sinatra::Base
+  post "/" do
+    if params["To"].present? && match = params["To"].match(/sip:(\d+)@collectiveidea.sip.twilio.com/)
+      builder do |xml|
+        xml.Response do |r|
+          r.Dial match[1]
+        end
+      end
+    else
+      hangup
+    end
+  end
 
+  def hangup
+    builder do |xml|
+      xml.Response do |r|
+        r.Hangup
+      end
+    end
+  end
 end
