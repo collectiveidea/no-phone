fa834b1c113d18e7825cb083e52a8a9b961b960a
Daniel Morrison <daniel@collectiveidea.com>
2014-10-21 00:06:36 -0400

    Don’t limit ourselves to digits 555-555 might be ok, for example.

--- no_phone.rb
+++ no_phone.rb
@@ -2,7 +2,7 @@ class NoPhone < Sinatra::Base
   post "/" do
     halt 404 unless params["To"].is_a?(String)
 
-    if params["CallStatus"] == "ringing" && match = params["To"].match(/sip:(\+?\d+)@collectiveidea.sip.twilio.com/)
+    if params["CallStatus"] == "ringing" && match = params["To"].match(/sip:(.+)@collectiveidea.sip.twilio.com/)
       builder do |xml|
         xml.Response do |r|
           r.Dial match[1], callerId: "+1-616-499-2122"
