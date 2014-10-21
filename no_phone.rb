0e2e0214e319618a1ac8aeab15d42c6c9d6626dc
Daniel Morrison <daniel@collectiveidea.com>
2014-10-20 22:04:32 -0400

    Very basic SIP testing app

--- no_phone.rb
+++ no_phone.rb
@@ -1,3 +1,21 @@
 class NoPhone < Sinatra::Base
+  post "/" do
+    if params["To"].present? && match = params["To"].match(/sip:(\d+)@#{Regexp.escape(ENV["TWILIO_SIP_ENDPOINT"])}/)
+      builder do |xml|
+        xml.Response do |r|
+          r.Dial match[1]
+        end
+      end
+    else
+      hangup
+    end
+  end
 
+  def hangup
+    builder do |xml|
+      xml.Response do |r|
+        r.Hangup
+      end
+    end
+  end
 end
