5705a0ac83649143b1c7cbe9d14863fc8c2d221a
Daniel Morrison <daniel@collectiveidea.com>
2014-10-20 22:20:04 -0400

    404 if no To set

--- no_phone.rb
+++ no_phone.rb
@@ -1,6 +1,8 @@
 class NoPhone < Sinatra::Base
   post "/" do
-    if params["To"].present? && match = params["To"].match(/sip:(\d+)@#{Regexp.escape(ENV["TWILIO_SIP_ENDPOINT"])}/)
+    halt 404 unless params["To"].is_a?(String)
+
+    if match = params["To"].match(/sip:(\d+)@#{Regexp.escape(ENV["TWILIO_SIP_ENDPOINT"])}/)
       builder do |xml|
         xml.Response do |r|
           r.Dial match[1]
